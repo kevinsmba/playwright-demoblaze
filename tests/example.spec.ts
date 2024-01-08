443711eb8ba51bf2f252b7d2fadbb7e8bd19df28
kevinsmba <kevinsmba@gmail.com>
2024-01-08 11:17:47 -0500

    assertion adding wih the symbol >

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,20 +1,8 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
-
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-});
-
-//bad
+test('Given I visit the DemoBlaze homepage', async ({ page }) => {
+  await page.goto('https://www.demoblaze.com/');
+  await page.locator('.list-group-item:text("Phones")').click();
+  const locator = page.locator('#tbodyid > div');
+  await expect(locator).toHaveCount(7);
+})
