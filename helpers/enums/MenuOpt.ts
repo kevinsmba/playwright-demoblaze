9aac63ae1c13b4b6e74728a31c9b79411ae7d5c5
kevinsmba <kevinsmba@gmail.com>
2024-01-10 16:05:39 -0500

    adding the objects and structure for Typescript

---
+++ helpers/enums/MenuOpt.ts
@@ -0,0 +1,3 @@
+export enum MenuOptions {
+    CART = "Cart"
+}
--- tests/AddToCart.spec.ts
+++ tests/AddToCart.spec.ts
@@ -1,5 +1,6 @@
 import { test, expect } from '@playwright/test';
 import { Products } from '../helpers/enums/products';
+import { MenuOptions } from '../helpers/enums/MenuOpt';
 
 
 test('Add a product to the cart from the Homepage', async ({ page }) => {
@@ -8,13 +9,13 @@ test('Add a product to the cart from the Homepage', async ({ page }) => {
         await page.goto('https://www.demoblaze.com/');
     })
     await test.step('When the Samsung galaxy s6 product is selected', async () => {
-/*        page.on('dialog', async dialog => {
-            expect(dialog.message()).toContain('Product added wq323')
-            await dialog.dismiss();
-        })*/
+        //page.on('dialog', async dialog => console.log(dialog.message()));
         await page.getByRole('link', { name: Products.GALAXY_S6, exact: true }).click();
     })
     await test.step('And the product is added to the cart', async () => {
-        await page.getByRole('link', { name: "Add to cart", exact: true }).click();
+        await page.getByRole('link', { name: "Add to cart"}).click();
+    })
+    await test.step('And goes to the cart page',async () => {
+        await page.getByRole('link', {name: MenuOptions.CART, exact: true}).click();
     })
 })
